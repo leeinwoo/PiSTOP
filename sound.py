c0e5ccb0c41dfaaa2890d175291bc3ff46483229
chatnoir0327 <chatnoir0327@naver.com>
2016-05-30 12:16:58 +0900

    사운드 소스
    사운드 소스 함수화 및 소리 제어 test

--- sound.py
+++ sound.py
@@ -1,11 +1,14 @@
-import pygame
-import time
- 
-pygame.init()
-#파일 로드
-pygame.mixer.music.load('사운드 파일명')
-#로드 된 파일 재생
-pygame.mixer.music.play()
-time.sleep(10)
-#사운드 정지
-pygame.mixer.music.stop()
+import pygame, sys, time
+
+
+def play_sound(filename, length = 2):
+    pygame.mixer.init()
+    pygame.mixer.music.load(filename + ".mp3")
+    pygame.mixer.music.play(length)
+    time.sleep(length)
+    pygame.mixer.music.stop()
+
+play_sound("stop")
+play_sound("test1")
+
+
