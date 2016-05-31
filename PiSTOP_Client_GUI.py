0d5ac2d8633333d6018c33dcdfb808fd639bb777
leeinwoo <leeinwoo2@naver.com>
2016-05-31 21:50:13 +0900

    클릭 이벤트 추가/수정

--- PiSTOP_Client_GUI.py
+++ PiSTOP_Client_GUI.py
@@ -10,6 +10,7 @@ select_frame=0
 
 cardLabels=[0 for i in range(7)] # 손 카드 이미지 레이블
 selectLabels=[0 for i in range(3)] # 선택 카드 이미지 레이블
+throwLabel=0
 
 bg_color = '#50AF49' #배경색 RGB CODE
 
@@ -21,8 +22,8 @@ def init_gui():
     root = Tk() #최상위 위젯 생성
     root.title("PiSTOP - ACTION GOSTOP GAME") #타이틀 설정
     
-    ws = root.winfo_screenwidth()*0.3 #스크린 넓이
-    hs = root.winfo_screenheight()*0.4 #스크린 높이
+    ws = root.winfo_screenwidth() #스크린 넓이
+    hs = root.winfo_screenheight() #스크린 높이
     set_window() #윈도우 창 크기 설정
     
     main_frame = Frame(root, background=bg_color) #메인 프레임 생성(최상위 위젯에 붙임)
@@ -37,7 +38,7 @@ def init_gui():
 
 #창 크기 설정
 def set_window():
-    root.geometry('%dx%d+%d+%d' % (340, 240, 0, 0))
+    root.geometry('%dx%d+%d+%d' % (ws, hs, 0, -30))
 
 #카드 프레임 생성
 def create_frame():
@@ -53,11 +54,16 @@ def create_label():
     for i in range(7):
         my_card = Label(card_frame, bg=bg_color)
         cardLabels[i] = my_card
-        cardLabels[i].grid(row=4-int(i/4), column=i%4)
+        cardLabels[i].grid(row=4-int(i/4), column=i%4)        
+
     for i in range(3):
         select_card = Label(select_frame, bg=bg_color)
         selectLabels[i] = select_card
         selectLabels[i].grid(row=0, column=i)
+
+    global throwLabel
+    throwLabel = Label(main_frame, bg=bg_color)
+    throwLabel.pack(expand=1)
 
 #카드 이미지 로드
 def get_image():
@@ -68,9 +74,25 @@ def get_image():
             image_count += 1            
         except :            
             continue            
-        image[num] = image[num].subsample(2,2)
+        #image[num] = image[num].subsample(2,2)
     print("%d개의 이미지를 로드했습니다." % image_count)
 
+#card image zoom
+def zoom_image(multi):    
+    for num in range(len(image)):
+        try:
+            image[num] = image[num].zoom(multi, multi)
+        except:
+            continue
+        
+#card image subsample
+def subsample_image(sub):
+    for num in range(len(image)):
+        try:
+            image[num] = image[num].subsample(sub, sub)
+        except:
+            continue
+    
 #카드 이미지 갱신
 def update_card(card_list):
     print(len(card_list))
@@ -82,6 +104,7 @@ def update_card(card_list):
     for i in range(len(card_list)):
         cardLabels[i].config(image=image[card_list[i]])
         cardLabels[i].grid(row=4-int(i/4), column=i%4)
+        cardLabels[i].bind("<Button-1>",onclick)
 
 #전체 이미지 삭제
 def clear():
@@ -89,16 +112,18 @@ def clear():
         cardLabels[i].grid_forget()
     for i in range(3):
         selectLabels[i].grid_forget()
+    throwLabel.pack_forget()
 
 #카드 선택
 def select_card(select1, select2, select3=0):
+    zoom_image(2)
     selectLabels[0].config(image=image[select1])
     selectLabels[0].grid(row=0, column=0)
 
     selectLabels[1].config(image=image[select2])
     selectLabels[1].grid(row=0, column=1)
 
-    if(select3!=0):
+    if(select3!=0):        
         selectLabels[2].config(image=image[select3])
         selectLabels[2].grid(row=0, column=2)
 
@@ -109,14 +134,30 @@ def select_answer(select):
         selectLabel.pack(expand=1)
         selectLabel.bind("<Button-1>",onclick)
 
+#click event
 def onclick(event):
-    print("onclick")
+    global card
+    print("x: %d, \t y: %d" % (event.x_root, event.y_root))
+    for i in range(len(card)):
+        if(event.widget==cardLabels[i]):
+            print(i)
+            print(event.widget)
+            print(cardLabels[i])
+            clear()
+            get_throw_card(card[i])
+
+#get throw card
+def get_throw_card(card):
+    global throwLabel
+    throwLabel.config(image=image[card])
+    throwLabel.pack()
+        
 
 init_gui()
-card=[15, 17, 27, 37, 47, 125, 126]
+card=[15, 17, 27, 37, 47, 125]
 update_card(card)
-clear()
-select_card(15,17)
-clear()
-select_answer(1)
+#clear()
+#select_card(15,17, 27)
+#clear()
+#select_answer(1)
 root.mainloop()
