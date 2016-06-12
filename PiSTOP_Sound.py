533d11e394d17886aff1abb0e378d452769ce928
chatnoir0327 <chatnoir0327@naver.com>
2016-06-12 18:58:19 +0900

    사운드 소스 수정
    사운드 스레드로 동시 처리 되도록 set_filename 추가

--- PiSTOP_Sound.py
+++ PiSTOP_Sound.py
@@ -1,9 +1,20 @@
 import pygame, sys, time
 
 
-def play_sound(filename, length = 1):
+def play_sound(length = 1):
     pygame.mixer.init()
     pygame.mixer.music.load("./sound/" + filename + ".mp3")
     pygame.mixer.music.play(length)
     time.sleep(length)
     pygame.mixer.music.stop()
+
+def play_sound4(length = 4):
+    pygame.mixer.init()
+    pygame.mixer.music.load("./sound/" + filename + ".mp3")
+    pygame.mixer.music.play(length)
+    time.sleep(length)
+    pygame.mixer.music.stop()
+
+def set_filename(fn):
+    global filename
+    filename = fn
